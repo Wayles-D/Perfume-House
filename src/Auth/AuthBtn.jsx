52f254e0a652cb4fdcd819b072ba67d5293d5368
Danyel222 <waylesstores@gmail.com>
2025-03-03 03:38:21 +0100

    code-updated

--- src/Auth/AuthBtn.jsx
+++ src/Auth/AuthBtn.jsx
@@ -5,9 +5,8 @@ import SignOut from './SignOut'
 const AuthBtn = () => {
   return (
     <div>
-    <Link to="/SignIn" className="border border-purple-500 text-purple-500 px-4 py-2 rounded-lg">
-        Sign In
-      </Link>
+      <SignIn/>
+      <SignOut/>
     </div>
   )
 }
